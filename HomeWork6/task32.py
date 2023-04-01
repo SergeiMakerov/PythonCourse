399ec00644ffe6807822d8f29ebe777c0b458f9b
Sergei <makerovs@yandex.ru>
2023-04-01 19:57:12 +0700

    add home work 6

---
+++ HomeWork6/task32.py
@@ -0,0 +1,12 @@
+# Задача 32: Определить индексы элементов массива (списка), 
+# значения которых принадлежат заданному диапазону 
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+import random
+list_1 = [random.randint(0, 10) for i in range(20)]
+print(list_1)
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
